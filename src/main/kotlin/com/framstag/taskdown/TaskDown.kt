e6a529b8bcbbc59230d8a4821f86d301771b20af
Tim Teulings <Tim Teulings Framstag@users.noreply.github.com>
2021-10-25 21:18:16 +0200

    Fixed ANSI rendering under Windows, bash and Windows Terminal.

--- src/main/kotlin/com/framstag/taskdown/TaskDown.kt
+++ src/main/kotlin/com/framstag/taskdown/TaskDown.kt
@@ -2,8 +2,17 @@ package com.framstag.taskdown
 
 import com.framstag.taskdown.commands.*
 import com.github.ajalt.clikt.core.*
+import io.github.alexarchambault.windowsansi.WindowsAnsi
 
 fun main(args: Array<String>) {
+     val isWindows = System.getProperty("os.name")
+        .lowercase()
+        .contains("windows")
+
+    if (isWindows) {
+      WindowsAnsi.setup()        
+    }
+
     TaskDown()
         .subcommands(
             AddTask(),
